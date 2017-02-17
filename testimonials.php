188e19b66f67bcf899aa01705861fbbb3a23129d
Bryan Willis <willis.bryan@live.com>
2017-02-17 09:07:38 -0500

    Update testimonials.php

--- testimonials.php
+++ testimonials.php
@@ -2,7 +2,7 @@
   <?php while ( have_rows( 'testimonials' ) ) : the_row(); ?>
       <li class="cell-12_md-4">
         <div class="reviewer">
-          <div class="minion mbi-chris float_n_md-float-l" style="<?php // if ( get_sub_field( 'testimonial_image') ) { ?>background-image: url(<?php // the_sub_field( 'testimonial_image' ); ?>);<?php } ?>"></div>
+          <div class="minion mbi-chris float_n_md-float-l" style="<?php if ( get_sub_field( 'testimonial_image') ) { ?>background-image: url(<?php the_sub_field( 'testimonial_image' ); ?>);<?php } ?>"></div>
           <div class="reviewer-header">
             <h6><?php the_sub_field( 'testimonial_name' ); ?></h6>
             <p class="reviewer-location"><?php the_sub_field( 'testimonial_description' ); ?></p>
